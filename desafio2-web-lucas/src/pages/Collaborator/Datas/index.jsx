9e3e7899df22b3059dc711ee2a0f6168caf1b368
Lusques <lucas.carlos@toodoo.com.br>
2022-10-19 23:31:25 -0300

    page-data - table

--- desafio2-web-lucas/src/pages/Collaborator/Datas/index.jsx
+++ desafio2-web-lucas/src/pages/Collaborator/Datas/index.jsx
@@ -8,9 +8,88 @@ const Datas = () => {
         <h4 className="title mb-5 pb-4 text-primary-400">Dados Corporativos</h4>
       </div>
       <div className="d-flex gap-4">
-        <section className="home-container-left w-100">
-          <div className="data-vacation shadow-lg mb-4">oie</div>
-          <div className="data-benefits shadow-sm rounded-1">
+        <section className="data-container-left w-100">
+          <div className="data-vacation shadow-lg mb-4">
+            <div className="d-flex justify-content-between align-items-center p-4 w-100">
+              <h6 className="text-primary-400 m-0">Férias</h6>
+              <div>
+                <Button
+                  className="button-primary body-2 py-2 px-5"
+                  dataBsTarget=""
+                  modal=""
+                  onClick=""
+                  txtButton="+ Solicitar férias"
+                  type="button"
+                />
+              </div>
+            </div>
+            <table className="vacation-table d-block">
+              <thead className="d-block px-4 bg-primary w-100">
+                <tr className="w-100 d-flex">
+                  <th className="body-2-semibold text-light d-block py-3">
+                    Início previsto
+                    <span className="vacation-th-icon ms-1">
+                      <i class="bi bi-caret-down-fill"></i>
+                    </span>
+                  </th>
+                  <th className="body-2-semibold text-light d-block py-3">
+                    Retorno previsto
+                    <span className="vacation-th-icon ms-1">
+                      <i class="bi bi-caret-down-fill"></i>
+                    </span>
+                  </th>
+                  <th className="body-2-semibold text-light d-block py-3 flex-grow-1 m-0">
+                    Status
+                    <span className="vacation-th-icon ms-1">
+                      <i class="bi bi-caret-down-fill"></i>
+                    </span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="d-block px-4">
+                <tr className="d-flex">
+                  <td className="body-2 text-gray-500 d-block py-3">
+                    14/05/2020
+                  </td>
+                  <td className="body-2 text-gray-500 d-block py-3">
+                    14/05/2020
+                  </td>
+                  <td className="body-2 text-light d-block py-3 flex-grow-1 m-0">
+                    <span className="bg-alert-success py-1 px-3 rounded-1 overline-2 fw-600">
+                      Aprovado
+                    </span>
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="body-2 text-gray-500 d-block py-3">
+                    14/05/2020
+                  </td>
+                  <td className="body-2 text-gray-500 d-block py-3">
+                    14/05/2020
+                  </td>
+                  <td className="body-2 text-light d-block py-3 flex-grow-1 m-0">
+                    <span className="bg-alert-warning-dark py-1 px-3 rounded-1 overline-2 fw-600">
+                      Em análise
+                    </span>
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="body-2 text-gray-500 d-block py-3">
+                    14/05/2020
+                  </td>
+                  <td className="body-2 text-gray-500 d-block py-3">
+                    14/05/2020
+                  </td>
+                  <td className="body-2 text-light d-block py-3 flex-grow-1 m-0">
+                    <span className="bg-alert-error py-1 px-3 rounded-1 overline-2 fw-600">
+                      Reprovado
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="data-benefits p-4 shadow-sm rounded-1">
             <h6 className="text-primary-400 mb-4">Benefícios</h6>
             <ul className="p-0 m-0">
               <li className="d-flex justify-content-between py-2 my-1">
